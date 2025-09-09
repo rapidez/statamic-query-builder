4937a9df9bb692c6db4cd82a339b3e2ba7e5f4ba
Bob <bob@justbetter.nl>
2025-09-09 14:47:01 +0200

    more compatibility

--- resources/views/templates/listing.blade.php
+++ resources/views/templates/listing.blade.php
@@ -1,8 +1,8 @@
-@if($product_query_builder->value()['hash'])
+@if($hash = $product_query_builder->value()['hash'])
     <x-rapidez::listing
-        item_list_id="query_builder_"
-        item_list_name="query_builder_"
-        v-bind:base-filters="() => [window.config.productlist['{{ $product_query_builder->value()['hash'] }}']]"
-        index="{{ $product_query_builder->value()['index'] }}"
+        item_list_id="query_builder_{{ $hash }}"
+        item_list_name="query_builder_{{ $hash }}"
+        v-bind:base-filters="() => [window.config.productlist['{{ $hash }}']]"
+        ::index="'{{ $product_query_builder->value()['index'] }}' ?? config.index.product"
     />
 @endif
--- resources/views/templates/slider.blade.php
+++ resources/views/templates/slider.blade.php
@@ -1,11 +1,11 @@
-<x-rapidez::productlist
-        :value="false"
-        v-bind:base-filters="() => [window.config.productlist['{{ $product_query_builder->value()['hash'] }}']]"
-        index="{{ $product_query_builder->value()['index'] }}"
-        
-    >
-    <x-slot:before>
-        <ais-configure :hits-per-page.camel="{{ $product_query_builder->value()['limit'] ?? 10 }}" />
-    </x-slot:before>
-</x-rapidez::productlist>
-
+@if($hash = $product_query_builder->value()['hash'])
+    <x-rapidez::productlist
+            :value="false"
+            v-bind:base-filters="() => [window.config.productlist['{{ $hash }}']]"
+            ::index="'{{ $product_query_builder->value()['index'] }}' ?? config.index.product"
+        >
+        <x-slot:before>
+            <ais-configure :hits-per-page.camel="{{ $product_query_builder->value()['limit'] ?? 10 }}" />
+        </x-slot:before>
+    </x-rapidez::productlist>
+@endif
