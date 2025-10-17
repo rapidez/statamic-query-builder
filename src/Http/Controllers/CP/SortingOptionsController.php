2aee6789d59e2be85bc95a7e083a2a62f4c38b09
Kevin Meijer <kevin@justbetter.nl>
2025-10-17 12:07:09 +0200

    RAP-1578 - Changed sort attributes + hide limit for specific templates

---
+++ src/Http/Controllers/CP/SortingOptionsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Rapidez\StatamicQueryBuilder\Http\Controllers\CP;
+
+use Illuminate\Http\JsonResponse;
+use Statamic\Http\Controllers\CP\CpController;
+
+class SortingOptionsController extends CpController
+{
+    public function index(): JsonResponse
+    {
+        $sorting = config('rapidez.searchkit.sorting', []);
+        $normalized = collect($sorting)
+            ->filter(function ($directions, $field) {
+                return is_string($field) && $field !== '';
+            })
+            ->map(function ($directions, $field) {
+                $label = ucwords(str_replace('_', ' ', strtolower($field)));
+                return [
+                    'label' => $label,
+                    'value' => $field,
+                ];
+            })
+            ->values()
+            ->all();
+
+        return response()->json($normalized);
+    }
+}
+
+
