c5cfe1d8b80d9131269672370067433712752676
Kevin Meijer <kevin@justbetter.nl>
2026-01-20 11:09:54 +0100

    WIP statamic 6 support

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue2';
+import statamic from '@statamic/cms/vite-plugin';
 
 export default defineConfig({
     plugins: [
+        statamic(),
         laravel({
             input: [
                 'resources/js/statamic-query-builder.js'
             ],
             publicDirectory: 'resources/dist',
         }),
-        vue(),
     ],
 });
