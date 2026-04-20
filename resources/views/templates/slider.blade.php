b518d8fc80f3cb659c61d19034db6b2f841da6ff
Kevin Meijer <kevin@justbetter.nl>
2026-04-20 12:43:13 +0200

    Updated frontend templates

--- resources/views/templates/slider.blade.php
+++ resources/views/templates/slider.blade.php
@@ -1,14 +1,18 @@
 @php
-$queryBuilder = $product_query_builder->value();
+if (is_array($product_query_builder)) {
+    $queryBuilder = $product_query_builder;
+} else {
+    $queryBuilder = $product_query_builder;
+}
 @endphp
 @if($hash = $queryBuilder['hash'])
     <x-rapidez::productlist
             :value="false"
             v-bind:base-filters="() => [window.config.productlist['{{ $hash }}']]"
-            ::index="'{{ $product_query_builder->value()['index'] }}' ?? config.index.product"
+            ::index="'{{ $queryBuilder['index'] }}' ?? config.index.product"
         >
         <x-slot:before>
-            <ais-configure :hits-per-page.camel="{{ $product_query_builder->value()['limit'] ?? 10 }}" />
+            <ais-configure :hits-per-page.camel="{{ $queryBuilder['limit'] ?? 10 }}" />
         </x-slot:before>
     </x-rapidez::productlist>
 @endif
